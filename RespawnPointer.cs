d0aef1f52a900e55b83f3d3e6ecd8202ca92c3d8
Ricardo Thiago Firmino <ricardothiagofirmino@hotmail.com>
2023-07-26 15:23:18 -0300

    Reestruturacao dos Scripts Principais

--- RespawnPointer.cs
+++ RespawnPointer.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -16,22 +15,3 @@ public class RespawnPointer : MonoBehaviour
         }
     }
 }
-=======
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class RespawnPointer : MonoBehaviour
-{
-    public Vector2 respawnPosition;
-
-    void OnTriggerEnter2D(Collider2D col)
-    {
-        if (col.gameObject.tag == "Respawn")
-        {
-            respawnPosition = new Vector2(col.gameObject.transform.position.x, col.gameObject.transform.position.y);
-            Debug.Log("Respawn Mudou");
-        }
-    }
-}
->>>>>>> a8399fb (Inicio Do Jogo: Movimentacao/Correr/Dano/Respawn)

dbc7726c761a8ce5c0d97f2cb79a9d567a9bdc33
Ricardo Thiago Firmino <ricardothiagofirmino@hotmail.com>
2023-09-10 19:14:05 -0300

    Pass Time Menu

--- Managers/PauseMenu.cs
+++ Managers/PauseMenu.cs
@@ -6,9 +6,22 @@ using UnityEngine.SceneManagement;
 public class PauseMenu : MonoBehaviour
 {
     public static bool GameIsPaused = false;
-    public GameObject inGameTime;
-
+    
+    // UI
+    [Header("UI Assets")]
     public GameObject pauseMenuUI;
+    public GameObject optionsMenu;
+    public GameObject saveMenu;
+    public GameObject loadMenu;
+    public GameObject quitMenu;
+
+    // Options Menu UI
+    [Header("Options Menu UI")]
+    public GameObject optionsGameplayUI;
+    public GameObject optionsControllsUI;
+    public GameObject optionsGraficsUI;
+    public GameObject optionsAudioUI;
+
 
     void Update()
     {
@@ -20,23 +33,200 @@ public class PauseMenu : MonoBehaviour
             }
             else
             {
-                Pause();
+                OpenPauseMenu();
             }
         }
     }
 
+
+    // --------- Pause Menu Buttoms ----------
+
     public void Resume()
     {
+        GoBackPauseMenu();
+
         pauseMenuUI.SetActive(false);
         Time.timeScale = 1f;
         GameIsPaused = false;
     }
-    void Pause()
+
+    void OpenPauseMenu()
     {
         pauseMenuUI.SetActive(true);
         Time.timeScale = 0f;
         GameIsPaused = true;
     }
+
+    public void ClosePauseMenu()
+    {
+        pauseMenuUI.SetActive(false);
+    }
+
+
+    // ---- Open Sub Menus ----
+
+    public void OpenOptionsMenu()
+    {
+        optionsMenu.SetActive(true);
+        ClosePauseMenu();
+    }
+
+    public void OpenSaveMenu()
+    {
+        saveMenu.SetActive(true);
+        ClosePauseMenu();
+    }
+
+    public void OpenLoadMenu() 
+    {
+        loadMenu.SetActive(true);
+        ClosePauseMenu();
+    }
+
+    public void OpenQuitMenu()
+    {
+        quitMenu.SetActive(true);
+        ClosePauseMenu();
+    }
+
+    // Go Back To Pause Menu And Close 
+    public void GoBackPauseMenu()
+    {
+        if (optionsMenu.active)
+        {
+            GoBackToOptions();
+
+            optionsMenu.SetActive(false);
+        }
+
+        if (saveMenu.active)
+        {
+            saveMenu.SetActive(false);
+        }
+
+        if (loadMenu.active)
+        {
+            loadMenu.SetActive(false);
+        }
+
+        if (quitMenu.active)
+        {
+            quitMenu.SetActive(false);
+        }
+
+        pauseMenuUI.SetActive(true);
+    }
+
+
+
+    // --- Options Menu Buttons ---
+
+    public void OpenOptionsGraphicsMenu()
+    {
+        if (optionsAudioUI.active)
+        {
+            optionsAudioUI.SetActive(false);
+        }
+
+        if (optionsControllsUI.active)
+        {
+            optionsControllsUI.SetActive(false);
+        }
+
+        if (optionsGameplayUI.active)
+        {
+            optionsGameplayUI.SetActive(false);
+        }
+
+        optionsGraficsUI.SetActive(true);
+    }
+
+    public void OpenOptionsGameplayMenu()
+    {
+        if (optionsAudioUI.active)
+        {
+            optionsAudioUI.SetActive(false);
+        }
+
+        if (optionsControllsUI.active)
+        {
+            optionsControllsUI.SetActive(false);
+        }
+
+        if (optionsGraficsUI.active)
+        {
+            optionsGraficsUI.SetActive(false);
+        }
+
+        optionsGameplayUI.SetActive(true);
+    }
+
+    public void OpenOptionsControllsMenu()
+    {
+        if (optionsAudioUI.active)
+        {
+            optionsAudioUI.SetActive(false);
+        }
+
+        if (optionsGameplayUI.active)
+        {
+            optionsGameplayUI.SetActive(false);
+        }
+
+        if (optionsGraficsUI.active)
+        {
+            optionsGraficsUI.SetActive(false);
+        }
+
+        optionsControllsUI.SetActive(true);
+    }
+
+    public void OpenOptionsAudioMenu()
+    {
+        if (optionsControllsUI.active)
+        {
+            optionsControllsUI.SetActive(false);
+        }
+
+        if (optionsGameplayUI.active)
+        {
+            optionsGameplayUI.SetActive(false);
+        }
+
+        if (optionsGraficsUI.active)
+        {
+            optionsGraficsUI.SetActive(false);
+        }
+
+        optionsAudioUI.SetActive(true);
+    }
+
+    public void GoBackToOptions()
+    {
+        if (optionsControllsUI.active)
+        {
+            optionsControllsUI.SetActive(false);
+        }
+
+        if (optionsGameplayUI.active)
+        {
+            optionsGameplayUI.SetActive(false);
+        }
+
+        if (optionsGraficsUI.active)
+        {
+            optionsGraficsUI.SetActive(false);
+        }
+
+        if (optionsAudioUI.active)
+        {
+            optionsAudioUI.SetActive(false);
+        }
+
+    }
+
+
+    // --- Quit Menu Buttons ---
 
     public void LoadMenu()
     {
@@ -52,3 +242,4 @@ public class PauseMenu : MonoBehaviour
     }
 
 }
+
